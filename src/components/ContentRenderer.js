274ba3bd5201c3f4dccd7102f2d1aad73bb5eda2
Eugene <eugsinsh@gmail.com>
2026-05-22 18:22:36 +0300

    create blocks

--- src/components/ContentRenderer.js
+++ src/components/ContentRenderer.js
@@ -1,65 +1,33 @@
 import { useLanguage } from '../context/LanguageContext';
 import { spacing } from '../styles/theme';
-import {
-  CardBlock,
-  EyebrowBlock,
-  GifBlock,
-  ImageBlock,
-  LanguageSwitcherBlock,
-  LinkBlock,
-  ListBlock,
-  NavigationBlock,
-  SubtitleBlock,
-  TextContentBlock,
-  TitleBlock,
-  VideoBlock,
-} from './blocks';
+import { blockRegistry } from './blocks/registry';
 
 export default function ContentRenderer({ content, lang = 'ru' }) {
   const { setLang } = useLanguage();
-  
-  // Фильтруем контент - исключаем heroImage
   const filteredContent = content.filter(item => item.type !== 'heroImage');
-  let hadHeroImage = content.some(item => item.type === 'heroImage');
+  const hadHeroImage = content.some(item => item.type === 'heroImage');
 
-  // Компонент для отрисовки конкретного блока контента
   const renderBlock = (item, index, heroOverlapStyle) => {
-    switch (item.type) {
-      case 'title':
-        return <TitleBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'eyebrow':
-        return <EyebrowBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'subtitle':
-        return <SubtitleBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'text':
-        return <TextContentBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'languageSwitcher':
-        return <LanguageSwitcherBlock key={index} item={item} lang={lang} setLang={setLang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'navigationButtons':
-        return <NavigationBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'list':
-        return <ListBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'image':
-        return <ImageBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'gif':
-        return <GifBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'video':
-        return <VideoBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'link':
-        return <LinkBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      case 'card':
-      case 'cardBig':
-      case 'cardSmall':
-        return <CardBlock key={index} item={item} lang={lang} heroOverlapStyle={heroOverlapStyle} />;
-      default:
-        return null;
+    const BlockComponent = blockRegistry[item.type];
+
+    if (!BlockComponent) {
+      return null;
     }
+
+    return (
+      <BlockComponent
+        key={index}
+        item={item}
+        lang={lang}
+        setLang={setLang}
+        heroOverlapStyle={heroOverlapStyle}
+      />
+    );
   };
 
   return (
     <>
       {filteredContent.map((item, index) => {
-        // Если это первый элемент и была heroImage, применяем наложение
         const isFirstAfterHero = index === 0 && hadHeroImage;
         const heroOverlapStyle = isFirstAfterHero ? { marginTop: -spacing.xxl } : {};
 
---
+++ src/components/blocks/registry.js
@@ -0,0 +1,26 @@
+import { CardBlock } from './CardBlock';
+import { GifBlock, ImageBlock } from './ImageBlock';
+import { LanguageSwitcherBlock } from './LanguageSwitcherBlock';
+import { LinkBlock } from './LinkBlock';
+import { ListBlock } from './ListBlock';
+import { NavigationBlock } from './NavigationBlock';
+import { EyebrowBlock, SubtitleBlock, TextContentBlock, TitleBlock } from './TextBlock';
+import { VideoBlock } from './VideoBlock';
+
+export const blockRegistry = {
+  title: TitleBlock,
+  eyebrow: EyebrowBlock,
+  subtitle: SubtitleBlock,
+  text: TextContentBlock,
+  languageSwitcher: LanguageSwitcherBlock,
+  navigationButtons: NavigationBlock,
+  list: ListBlock,
+  image: ImageBlock,
+  gif: GifBlock,
+  video: VideoBlock,
+  link: LinkBlock,
+  card: CardBlock,
+  cardBig: CardBlock,
+  cardSmall: CardBlock,
+};
+
